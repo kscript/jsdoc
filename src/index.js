62670cf445c51d8f5b8ffb894733fc8db8831407
kscript <1258@live.cn>
2018-10-13 18:59:30 +0800

    添加一个jsdoc demo

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/**
+ * 测试类
+ * @class
+ */
+class Person {
+  constructor(){
+
+  }
+  /**
+   * 测试方法
+   * @func
+   * @param key {string} key 
+   * @param value {any} value 
+   */
+  setter (key, value){
+      
+  }
+}
